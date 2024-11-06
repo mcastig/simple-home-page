e707a9dfa86be5cb9ec952457d76fbd5876886de
Maximino Castillo <maxcastgarcia@gmail.com>
2024-11-06 03:41:34 -0600

    Add comment code with explaning all methods

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,27 +1,52 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ModeToggleComponent } from "../mode-toggle/mode-toggle.component";
+import { ModeToggleComponent } from '../mode-toggle/mode-toggle.component';
 
+/**
+ * Header component for the application.
+ *
+ * This component handles the overall layout of the header, including the navigation bar,
+ * theme toggle, and offcanvas menu.
+ */
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [CommonModule, ModeToggleComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  /**
+   * Indicates whether the dark theme is currently active.
+   */
   isDarkTheme: boolean = false;
+
+  /**
+   * Indicates whether the offcanvas menu is currently displayed.
+   */
   isDisplayMenu: boolean = false;
 
+  /**
+   * Handles the theme change event emitted by the `ModeToggleComponent`.
+   * Updates the `isDarkTheme` property accordingly.
+   *
+   * @param $event The new theme state (true for dark, false for light).
+   */
   changeColorTheme($event: boolean) {
     this.isDarkTheme = $event;
   }
 
+  /**
+   * Returns a class object to be used for the navbar toggler icon.
+   * The classes are dynamically determined based on the `isDarkTheme` property.
+   *
+   * @returns An object containing the classes to be applied.
+   */
   getTogglerClasses() {
     return {
-      'navbar-toggler-icon': true,
-      'dark-icon': this.isDarkTheme,
-      'light-icon': !this.isDarkTheme
+      'navbar-toggler-icon': true, // Always apply the base class
+      'dark-icon': this.isDarkTheme, // Apply the dark icon class if the theme is dark
+      'light-icon': !this.isDarkTheme // Apply the light icon class if the theme is light
     };
   }
 }
